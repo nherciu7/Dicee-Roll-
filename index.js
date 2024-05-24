65edcd5ca1d42e7c37b5d45bcfadcf8f62ba2142
Nichita Herciu <nichitaherciu2003@gmail.com>
2024-05-24 12:01:11 +0300

    Final commit of my project

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+document.getElementById('img1').setAttribute("src" , "./images/dice" + randomNumber1 + ".png");
+document.getElementById('img2').setAttribute("src" , "./images/dice" + randomNumber2 + ".png");
+document.getElementById('refreshButton').addEventListener('click', function() {
+  location.reload();
+});
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else if(randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML = "It's a draw!"
+}
+
